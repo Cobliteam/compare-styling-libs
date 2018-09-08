45d373d3349eaa820fcf5bc69ca65c3ec98071f4
Fernando Abreu <fernandoabreu2@hotmail.com>
2018-09-07 21:55:51 -0300

    Configure storybook and add example

---
+++ stories/button.stories.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+
+const stories = storiesOf("Button", module);
+
+stories.add("css-modules", () => (
+  <button> HI </button>
+));
